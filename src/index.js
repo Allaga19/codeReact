954a4bf5d3ca87ce1f41f9a8188516a7bba625c8
Allaga <Allaga59@yandex.ru>
2021-05-13 07:23:36 +0300

    com

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import PropTypes from 'prop-types';
 import './style.css';
 import { App } from './components';
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// const Hello = ({name, children}) => (
+//    <div>
+//       Hello, {name}!
+//       {children}
+//    </div>
+// )
+
+// Hello.propTypes = {
+//    name: PropTypes.string.isRequired
+// }
+
+// const app = (
+//    <Fragment>
+//       <Hello name="Igor"></Hello>
+//       <Hello name="Ivan"></Hello>
+//       <Hello><div>Test</div></Hello>
+//    </Fragment>
+// )
+
+ReactDOM.render(App, document.getElementById('root'))
   
 
 
