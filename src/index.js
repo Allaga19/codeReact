3bcc5f4bd1d62887bffe468f3f644dce6709beed
Allaga <Allaga59@yandex.ru>
2021-05-14 05:59:29 +0300

    #4 Свойства компонентов

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import './style.css';
-import { App } from './components';
+// import { App } from './components';
 
+// карточка товара
+const Product = ({ photo, header, price, description, click }) => (
+   <div className="product" onClick={click}>
+      <img className="product__photo" src={photo} alt=""/>
+      <div className="product__information">
+         <h2 className="product__header">{header}</h2>
+         <div className="product__price">Цена: {price} рублей</div>
+         <div className="product__description">{description}</div>
+      </div>
+   </div>
+)
+Product.propTypes = {
+   photo: PropTypes.string,
+   header: PropTypes.string,
+   price: PropTypes.number,
+   description: PropTypes.string,
+   click: PropTypes.func
+}
+Product.defaultProps = {
+   photo: '/img/0.jpg',
+   header: 'Неизвестный продукт',
+   price: '0',
+   description: '...',
+   click: () => alert('Неизвестный продукт')
+}
 // const Hello = ({name, children}) => (
 //    <div>
 //       Hello, {name}!
@@ -14,16 +39,46 @@ import { App } from './components';
 // Hello.propTypes = {
 //    name: PropTypes.string.isRequired
 // }
+// // значение по умолчанию
+// Hello.defaultProps = {
+//    name: 'no name'
+// }
 
-// const app = (
-//    <Fragment>
-//       <Hello name="Igor"></Hello>
-//       <Hello name="Ivan"></Hello>
-//       <Hello><div>Test</div></Hello>
-//    </Fragment>
-// )
+const app = (
+   <Fragment>
+      <Product/>
+      <Product
+         photo="/img/3.jpg"
+         header="Кросовки"
+         price={1000}
+         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+         click={()=> alert('Кросовки')}
+      />
+       <Product
+         photo="/img/1.jpg"
+         header="Фотоаппарат"
+         price={4000}
+         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+         click={()=> alert('Фотоаппарат')}
+      />
+       <Product
+         photo="/img/2.jpg"
+         header="Наушники"
+         price={2000}
+         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+         click={()=> alert('Наушники')}
+      />
+       <Product
+         photo="/img/4.jpg"
+         header="Часы"
+         price={3000}
+         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+         click={()=> alert('Часы')}
+      />
+   </Fragment>
+)
 
-ReactDOM.render(App, document.getElementById('root'))
+ReactDOM.render(app, document.getElementById('root'))
   
 
 
